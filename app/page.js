375fb7141198d903214e0621607ec61d4449f7fb
AbdelrahmanEhab <abdoehab5008@gmail.com>
2025-02-01 17:38:20 +0200

    Latest Updates Tv Show Next App

--- app/page.js
+++ app/page.js
@@ -1,100 +1,92 @@
+// pages/index.js
+import Link from "next/link";
 import Image from "next/image";
+import {
+  FaDatabase,
+  FaSpinner,
+  FaExclamationTriangle,
+  FaBars,
+  FaReact,
+  FaServer,
+} from "react-icons/fa";
 
-export default function Home() {
+export default function HomePage() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+    <div className="relative min-h-screen bg-black">
+      {/* Background image using Next.js Image Component */}
+      <div className="relative h-screen w-full overflow-hidden">
         <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
+          src="https://cdn.pixabay.com/photo/2023/12/12/13/05/godzilla-8445254_1280.jpg"
+          alt="TV Show Spotlight"
+          layout="fill"
+          objectFit="cover"
           priority
+          className="opacity-75"
         />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+        {/* Transparent overlay */}
+        <div className="absolute inset-0 bg-black bg-opacity-50 flex flex-col justify-center items-center text-center px-4">
+          <h1 className="text-4xl font-bold text-white">
+            Discover Amazing TV Shows
+          </h1>
+          <p className="text-lg mt-3 text-white">
+            Dive into the world of endless entertainment. Explore genres, follow
+            your favorite series, and never miss an episode!
+          </p>
+          <Link href="/shows">
+            <p className="mt-8 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+              Explore Shows
+            </p>
+          </Link>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+      </div>
+
+      {/* What You Will Learn Section */}
+      <div className="bg-gray-900 text-white py-8">
+        <div className="container mx-auto px-4">
+          <h2 className="text-3xl font-bold text-center">What You Will Try</h2>
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-6">
+            <div className="bg-white rounded-lg p-6 flex items-center shadow-lg">
+              <FaDatabase className="text-3xl text-blue-500 mr-4" />
+              <span className="font-semibold text-gray-800 text-lg">
+                Data Fetching
+              </span>
+            </div>
+            <div className="bg-white rounded-lg p-6 flex items-center shadow-lg">
+              <FaSpinner className="text-3xl text-blue-500 mr-4" />
+              <span className="font-semibold text-gray-800 text-lg">
+                Loading Techniques
+              </span>
+            </div>
+            <div className="bg-white rounded-lg p-6 flex items-center shadow-lg">
+              <FaExclamationTriangle className="text-3xl text-blue-500 mr-4" />
+              <span className="font-semibold text-gray-800 text-lg">
+                Error Handling
+              </span>
+            </div>
+            <div className="bg-white rounded-lg p-6 flex items-center shadow-lg">
+              <FaBars className="text-3xl text-blue-500 mr-4" />
+              <span className="font-semibold text-gray-800 text-lg">
+                Navigation
+              </span>
+            </div>
+            <div className="bg-white rounded-lg p-6 flex items-center shadow-lg">
+              <FaReact className="text-3xl text-blue-500 mr-4" />
+              <span className="font-semibold text-gray-800 text-lg">
+                Use of React Icons
+              </span>
+            </div>
+            <div className="bg-white rounded-lg p-6 flex items-center shadow-lg">
+              <FaServer className="text-3xl text-blue-500 mr-4" />
+              <span className="font-semibold text-gray-800 text-lg">
+                Server Data Fetching
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* Footer */}
+      <footer className="text-center py-8 text-sm text-white">
+        TV Show Central || Next JS Trials
       </footer>
     </div>
   );
